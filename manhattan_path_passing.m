34fae0725ccc8361f79a89d3ba590774f3f6bb62
anirudhsuresh <56661161+anirudhsuresh@users.noreply.github.com>
2020-06-11 18:14:18 -0700

    fixed bug

--- manhattan_path_passing.m
+++ manhattan_path_passing.m
@@ -16,7 +16,7 @@ if counter==1
     [final_pose,final_pred]=closed_loop_control(path,Ld,T,q_true,q_pred);
     
 else 
-    q_true=initial_pose;
+    q_true=[initial_pose(1);initial_pose(2);initial_pose(3);0;0];
     q_pred=initial_pred;
     X=[end_point(1),end_point(2);mid_point(1),mid_point(2)];
     d2=pdist(X,'euclidean');
